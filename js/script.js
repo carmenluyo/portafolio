4c44a456ec2964e96f4adf54e1214c24a313685d
carmenluyo <c.luyorosales@gmail.com>
2015-05-08 16:53:02 -0500

    hize el navegador responsive

--- js/script.js
+++ js/script.js
@@ -3,10 +3,35 @@ $(document).ready(function(){
 		var prueba = $(this).scrollTop();
 		$("#carmen").text(prueba);
 		if (prueba > 658) {
-			$(".navbar-default").addClass("navegacion");
+			$(".nav-efecto").addClass("navegacion");
 		}
 			else if (prueba < 658) {
-			$(".navbar-default").removeClass("navegacion");
+			$(".nav-efecto").removeClass("navegacion");
 		}
 	});
 });
+
+
+
+
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+ 
+
+}
